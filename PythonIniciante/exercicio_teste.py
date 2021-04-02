815229bb6c7665853f5ca3222ebd02d647b08496
LuizMoreira-py <luizmoreira98@gmail.com>
2021-04-02 14:17:56 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -1,16 +1,19 @@
-# Escreva um programa que pergunte a distância
-# que um passageiro deseja percorrer em km.
-# Calcule o preço da passagem, cobrando R$ 0,50 por km para
-# viagens de até de 200 km, e R$ 0,45 para viagens mais longas.
-
-distracia = float(input("Informe a distância(KM) que deseja percorrer: "))
-preco = 0
-
-if distracia <= 200:
-    preco = 0.50
-
+categoria = int(input("Digite a categoria do produto: "))
+if categoria == 1:
+    preco = 10
 else:
-    preco = 0.45
-
-custo = distracia * preco
-print(f"Valor total: R$ {custo:6.2f}")
+    if categoria == 2:
+        preco = 18
+    else:
+        if categoria == 3:
+            preco = 23
+        else:
+            if categoria == 4:
+                preco = 26
+            else:
+                if categoria == 5:
+                    preco = 31
+                else:
+                    print("Categoria inválida, digite um valor entre 1 e 5!")
+                    preco = 0
+print(f"O preço do produto é: R${preco:6.2f}")
