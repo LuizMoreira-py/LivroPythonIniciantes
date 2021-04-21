6af8ae63475ca2fe2eeb2c0738496d76653826b5
LuizMoreira-py <luizmoreira98@gmail.com>
2021-04-21 09:01:06 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -1,6 +1,8 @@
-fim = int(input("Digite o último número a imprimir: "))
-x = 1
+tabuada = int(input("Tabuada de: "))
+inicio = int(input("Informe o inicio da tabuada: "))
+fim = int(input("Informe o fim da tabuada: "))
+
+x = inicio
 while x <= fim:
-    if x % 2 == 3:
-        print(x)
+    print(tabuada * x)
     x = x + 1
