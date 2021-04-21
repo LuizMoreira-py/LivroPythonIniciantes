91af21c45fe04b923d4672d14e6e9c53e4cd0f79
LuizMoreira-py <luizmoreira98@gmail.com>
2021-04-21 19:54:07 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -1,12 +1,9 @@
-n1 = int(input("Digite um número inteiro: "))
-multi = int(input(f"Digite quantas vezes deseja somar o número {n1} x "))
+n1 = int(input("Digite o número que deseja dividir: "))
+n2 = int(input(f"Digite quantas vezes deseja dividir o número {n1}: "))
 
-n = 1
-soma = 0
-cont = 0
+div = n1
 
-while n <= multi:
-    soma = soma + n1
-    n = n + 1
-    cont += 1
-    print(f"Soma {cont}: {n1} + {soma}")
+
+while div >= 0:
+    div -= n2
+print(f"Total: {div}")
