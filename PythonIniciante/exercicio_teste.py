5ba29d71f93480ae675768ea27c58704a7c703fd
LuizMoreira-py <luizmoreira98@gmail.com>
2021-04-21 21:50:35 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -1,9 +1,10 @@
-n1 = int(input("Digite o número que deseja dividir: "))
-n2 = int(input(f"Digite quantas vezes deseja dividir o número {n1}: "))
+n1 = int(input("Informe um dividendo: "))
+n2 = int(input(f"Informe um divisor para o número {n1} / "))
 
 div = n1
+cont = 0
 
-
-while div >= 0:
+while div > 0:
     div -= n2
-print(f"Total: {div}")
+    cont += 1
+    print(f"Divisão {cont}: {div}")
