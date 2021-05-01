2b5b72b01678bec43f2e9045fcf932541953cf88
LuizMoreira-py <luizmoreira98@gmail.com>
2021-05-01 13:55:28 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -1,7 +1,12 @@
-x = 1
-soma = 0
-while x <= 5:
-    n = int(input(f"{x} Digite o número: "))
-    soma = soma + n
-    x += 1
-print(f"Média: {soma / 5:5.2f}")
+deposito = float(input("Digito o valor do depósito: "))
+taxa = float(input("Digite o valor da taxa: "))
+
+
+
+mes = 0
+total = deposito * taxa
+
+while mes <= 24:
+    juros = total * mes    
+    mes += 1
+    print(f"Mês {mes} Deposito {deposito} Rendimento {juros}")
