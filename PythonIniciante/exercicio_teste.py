ebe803b7b4d2ffb80605da2aefff20eb70bd6924
LuizMoreira-py <luizmoreira98@gmail.com>
2021-05-01 12:53:08 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -1,7 +1,7 @@
-n = 1
+x = 1
 soma = 0
-while n <= 10:
-    x = int(input(f"Digite o {n} número: "))
-    soma = soma + x
-    n += 1
-print(f"Soma: {soma}")
+while x <= 5:
+    n = int(input(f"{x} Digite o número: "))
+    soma = soma + n
+    x += 1
+print(f"Média: {soma / 5:5.2f}")
