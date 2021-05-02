6fb0af25546285482c32bed2e07b54d78f2f5a7a
LuizMoreira-py <luizmoreira98@gmail.com>
2021-05-02 18:49:47 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -7,8 +7,6 @@ rendimento = deposito * taxa
 
 while mes <= 23:
     rendimentoPeriodo = (deposito * taxa) * mes
+    rendiimentoTotal = deposito + rendimentoPeriodo
     mes += 1
-    print(f"Mês {mes} Depósito: R$ {deposito} Rendimento: R$ {rendimento} Rendimento periodo: R$ {rendimentoPeriodo}")
-
-total = deposito + rendimentoPeriodo
-print(f"Total: R$ {total}")
+    print(f"Mês {mes} ---- Depósito: R$ {deposito:6.2f} ---- Rendimento: R$ {rendimento:6.2f} ---- Rendimento periodo: R$ {rendimentoPeriodo:6.2f} ---- Rendimento total: {rendiimentoTotal:6.2f}")
