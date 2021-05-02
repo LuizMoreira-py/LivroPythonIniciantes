ce157b23e906c7ec86fbba838a640066dc1e9982
LuizMoreira-py <luizmoreira98@gmail.com>
2021-05-02 14:16:02 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -4,9 +4,9 @@ taxa = float(input("Digite o valor da taxa: R$ "))
 
 
 mes = 0
-total = deposito * taxa
 
 while mes <= 24:
-    juros = total * mes    
+    total = deposito * taxa
+    juros = total * mes
     mes += 1
-    print(f"Mês {mes} Deposito {deposito} Rendimento {juros}")
+    print(f"Mês {mes} Deposito {total} Rendimento {juros}")
