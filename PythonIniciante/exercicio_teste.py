e46ccc088cd0cbc6ab5128d7b26cccd02b4908c6
LuizMoreira-py <luizmoreira98@gmail.com>
2021-05-12 21:58:46 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -1,17 +1,4 @@
-# Altere o programa anterior de forma a perguntar também o valor depositado
-# mensalmente. Esse valor será depositado no início de cada mês, e você
-# deve considerá-lo para o cálculo de juros do mês seguinte.
+# Escreva um programa que pergunte o valor inicial de uma dívida e o juro
+# mensal. Pergunte também o valor mensal que será pago. Imprima o número
+# de meses para que a dívida seja paga, o total pago e o total de juros pago.
 
-deposito = float(input("Depósito inicial: R$ "))
-taxa = float(input("Ex: 3 para 3.0%: "))
-investimento = float(input("Depósito mesnsal: R$ "))
-
-mes = 0
-saldo = deposito
-
-while mes <= 23:
-    saldo = saldo + (saldo * (taxa / 100)) + investimento
-    mes += 1
-    print(f"Mês {mes} - Rendimento: R$ {saldo:6.2f}")
-
-print(f"Rendimento total: R$ {saldo - deposito:6.2f}")
