631bc4d2362ad6dec2eae4e54202117fb723f6a3
LuizMoreira-py <luizmoreira98@gmail.com>
2021-05-12 22:10:17 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -2,3 +2,20 @@
 # mensal. Pergunte também o valor mensal que será pago. Imprima o número
 # de meses para que a dívida seja paga, o total pago e o total de juros pago.
 
+divida = float(input("Valor divida: R$ "))
+juros = float(input("Ex: 3 para 3%: "))
+parcelas = float(input("Valor da parcela: R$ "))
+
+mes = 1
+
+if (divida * (juros / 100) > parcelas):
+    print("Com esse valor você nunca vai terminar de pagar")
+else:
+    total = divida
+    juros_total = 0
+
+    while total > parcelas:
+        juros = total * (juros / 100)
+        total = total + juros - parcelas
+        juros_total = juros_total + juros
+        mes += 1
