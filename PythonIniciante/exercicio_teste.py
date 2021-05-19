4aac0aa1cf989520e746f2ded9ac1abd4c7f7308
LuizMoreira-py <luizmoreira98@gmail.com>
2021-05-19 19:27:40 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -1,7 +1,7 @@
 tabuada = 1
 while tabuada <= 10:
-    numero = 1
+    numero = int(input("Informe um número: "))
     while numero <= 10:
         print(f"{tabuada} x {numero} = {tabuada * numero}")
-        numero += 1
+        # numero += 1
     tabuada += 1
