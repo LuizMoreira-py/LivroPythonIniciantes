445390e669ce27707e7f28b50a7d1cd678f79d9f
LuizMoreira-py <luizmoreira98@gmail.com>
2021-05-19 19:26:19 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -1,43 +1,7 @@
-valor = float(input("Digite o valor a pagar: "))
-cedulas = 0
-atual = 100.00
-apagar = valor
-
-while True:
-    if atual <= apagar:
-        apagar -= atual
-        cedulas += 1
-
-    else:
-        if atual >= 1:
-            print(f"{cedulas} cédula(s) de R${atual}")
-        else:
-            print(f"{cedulas} moeda(s) de R${atual:5.2f}")
-        if apagar == 0.01:
-            break
-        elif atual >= 100:
-            atual = 50
-        elif atual >= 50:
-            atual = 20
-        elif atual >= 20:
-            atual = 10
-        elif atual >= 10:
-            atual = 5
-        elif atual >= 5:
-            atual = 2
-        elif atual >= 2:
-            atual = 1
-        elif atual >= 1:
-            atual = 0.50
-        elif atual >= 0.50:
-            atual = 0.25
-        elif atual >= 0.25:
-            atual = 0.10
-        elif atual >= 0.10:
-            atual = 0.05
-        elif atual >= 0.05:
-            atual = 0.01
-        else:
-            atual = 0.01
-            break
-        cedulas = 0
+tabuada = 1
+while tabuada <= 10:
+    numero = 1
+    while numero <= 10:
+        print(f"{tabuada} x {numero} = {tabuada * numero}")
+        numero += 1
+    tabuada += 1
