044be1fc4fd1009b2b494a1fa0570b5e2bbcc55a
LuizMoreira-py <luizmoreira98@gmail.com>
2021-05-20 23:34:27 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -1,49 +1,6 @@
-# Escreva um programa que exiba uma lista de operações
-# (menu): adição, subtração, divisão, multiplicação e sair.
-# Imprima a tabuada da operação escolhida. Repita até que a
-# opção saída seja escolhida.
-
-saida = 1
-
-while saida == 1:
-    print("Menu: \n ")
-    print(" 1 - Adição \n 2 - Subtração \n 3 - Divisão \n 4 - Multiplicação")
-    print(" 5 - Saída \n")
-
-    operacao = int(input("Escolha uma operação: "))
-
-    if operacao == 1:
-        tabuada = 1
-        while tabuada <= 10:
-            numero = 1
-            while numero <= 10:
-                print(f"{tabuada} + {numero} = {tabuada + numero}")
-                numero += 1
-            tabuada += 1
-    if operacao == 2:
-        tabuada = 1
-        while tabuada <= 10:
-            numero = 1
-            while numero <= 10:
-                print(f"{tabuada} - {numero} = {tabuada - numero}")
-                numero += 1
-            tabuada += 1
-    if operacao == 3:
-        tabuada = 1
-        while tabuada <= 10:
-            numero = 1
-            while numero <= 10:
-                print(f"{tabuada} / {numero} = {tabuada // numero}")
-                numero += 1
-            tabuada += 1
-    if operacao == 4:
-        tabuada = 1
-        while tabuada <= 10:
-            numero = 1
-            while numero <= 10:
-                print(f"{tabuada} x {numero} = {tabuada * numero}")
-                numero += 1
-            tabuada += 1
-
-    if operacao == 5:
-        saida -= 1
+# Escreva um programa que leia um número e verifique se é ou não um
+# número primo. Para fazer essa verificação, calcule o resto da
+# divisão do número por 2 e depois por todos os números ímpares
+# até o número lido. Se o resto de uma dessas divisões for igual zero,
+# o número não é primo. Observe que 0 e 1 não são primos e que 2 é o único
+# número primo que é par.
