0c70181840a3ad5a74171d9455c3ff9807371875
LuizMoreira-py <luizmoreira98@gmail.com>
2021-05-20 22:52:39 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -1,43 +1,50 @@
-valor = float(input("Digite o valor a pagar: "))
-cedulas = 0
-atual = 100.00
-apagar = valor
+valor = 1
 
-while True:
-    if atual <= apagar:
-        apagar -= atual
-        cedulas += 1
+while valor >= 0:
+    valor = float(input("Digite o valor a pagar: "))
+    cedulas = 0
+    atual = 100.00
+    apagar = valor
+    if valor == 0:
+        break
 
-    else:
-        if atual >= 1:
-            print(f"{cedulas} cédula(s) de R${atual}")
+
+    while True:
+        if atual <= apagar:
+            apagar -= atual
+            cedulas += 1
+
         else:
-            print(f"{cedulas} moeda(s) de R${atual:5.2f}")
-        if apagar == 0.01:
-            break
-        elif atual >= 100:
-            atual = 50
-        elif atual >= 50:
-            atual = 20
-        elif atual >= 20:
-            atual = 10
-        elif atual >= 10:
-            atual = 5
-        elif atual >= 5:
-            atual = 2
-        elif atual >= 2:
-            atual = 1
-        elif atual >= 1:
-            atual = 0.50
-        elif atual >= 0.50:
-            atual = 0.25
-        elif atual >= 0.25:
-            atual = 0.10
-        elif atual >= 0.10:
-            atual = 0.05
-        elif atual >= 0.05:
-            atual = 0.01
-        else:
-            atual = 0.01
-            break
-        cedulas = 0
+            if atual >= 1:
+                print(f"{cedulas} cédula(s) de R${atual}")
+            else:
+                print(f"{cedulas} moeda(s) de R${atual:5.2f}")
+
+            if apagar == 0.01:
+                break
+            elif atual >= 100:
+                atual = 50
+            elif atual >= 50:
+                atual = 20
+            elif atual >= 20:
+                atual = 10
+            elif atual >= 10:
+                atual = 5
+            elif atual >= 5:
+                atual = 2
+            elif atual >= 2:
+                atual = 1
+            elif atual >= 1:
+                atual = 0.50
+            elif atual >= 0.50:
+                atual = 0.25
+            elif atual >= 0.25:
+                atual = 0.10
+            elif atual >= 0.10:
+                atual = 0.05
+            elif atual >= 0.05:
+                atual = 0.01
+            else:
+                atual = 0.01
+                break
+            cedulas = 0
