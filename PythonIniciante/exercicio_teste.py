751390a3464dcfc74ef16a1fa321f921b7534dbe
LuizMoreira-py <luizmoreira98@gmail.com>
2021-05-20 23:17:55 -0300

    Update exercicio_teste.py

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -19,6 +19,30 @@ while saida == 1:
                 print(f"{tabuada} + {numero} = {tabuada + numero}")
                 numero += 1
             tabuada += 1
+    if operacao == 2:
+        tabuada = 1
+        while tabuada <= 10:
+            numero = 1
+            while numero <= 10:
+                print(f"{tabuada} - {numero} = {tabuada - numero}")
+                numero += 1
+            tabuada += 1
+    if operacao == 3:
+        tabuada = 1
+        while tabuada <= 10:
+            numero = 1
+            while numero <= 10:
+                print(f"{tabuada} / {numero} = {tabuada // numero}")
+                numero += 1
+            tabuada += 1
+    if operacao == 4:
+        tabuada = 1
+        while tabuada <= 10:
+            numero = 1
+            while numero <= 10:
+                print(f"{tabuada} x {numero} = {tabuada * numero}")
+                numero += 1
+            tabuada += 1
 
     if operacao == 5:
-        break
+        saida -= 1
