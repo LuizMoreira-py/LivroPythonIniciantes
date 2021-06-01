cca12e4e9b544762c414833817690ff79430b66b
LuizMoreira-py <luizmoreira98@gmail.com>
2021-05-31 21:42:34 -0300

    commit

--- PythonIniciante/exercicio_teste.py
+++ PythonIniciante/exercicio_teste.py
@@ -5,25 +5,4 @@
 # o número não é primo. Observe que 0 e 1 não são primos e que 2 é o único
 # número primo que é par.
 
-n = int(input("Digite um número:"))
-if n < 0:
-    print("Número inválido. Digite apenas valores positivos")
-if n == 0 or n == 1:
-    print(f"{n} é um caso especial.")
-else:
-    if n == 2:
-        print("2 é primo")
-    elif n % 2 == 0:
-        print(f"{n} não é primo, pois 2 é o único número par primo.")
-    else:
-        x = 3
-        while(x < n):
-            if n % x == 0:
-                break
-            x = x + 2
-            if n % x != 0 and n % x != 1:
-                print(x)
-        if x == n:
-            print(f"{n} é primo")
-        else:
-            print(f"{n} não é primo, pois é divisível por {x}")
+
