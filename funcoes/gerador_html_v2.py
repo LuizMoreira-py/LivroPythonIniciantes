1ca5ee2b13d796f51e44fd5423a04d2b3c71b459
LuizMoreira-py <luizmoreira98@gmail.com>
2021-06-06 14:16:46 -0300

    commit

--- funcoes/gerador_html_v2.py
+++ funcoes/gerador_html_v2.py
@@ -1,12 +1,10 @@
 #!/usr/local/bin/python3
-def tag_bloco(texto, classe='success'):
-    return f'<div class="{classe}">{texto}</div>'
+def tag_bloco(texto, classe='success', inline=False):
+    tag = 'span' if inline else 'div'
+    return f'<{tag} class="{classe}">{texto}</{tag}>'
 
 
 if __name__ == '__main__':
-    # Teste (assertions)
-    assert tag_bloco('Incluído com sucesso!') == \
-        '<div class="success">Incluído com sucesso!</div>'
-    assert tag_bloco('Impossível excluir!', 'error') == \
-        '<div class="error">Impossível excluir!</div>'
     print(tag_bloco('bloco'))
+    print(tag_bloco('inline e classe', 'info', True))
+    print(tag_bloco('inline', inline=True))
