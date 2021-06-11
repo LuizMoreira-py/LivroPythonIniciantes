cac4bbfe694af25017600e0af4fdd19791e9dc7d
LuizMoreira-py <luizmoreira98@gmail.com>
2021-06-10 21:04:41 -0300

    Create aula4.py

---
+++ Udemy/Camila/aula4.py
@@ -0,0 +1,23 @@
+"""
+Tipos de dados
+str - string - 'Assim' "Assim"
+int - inteiro - 1234560 - 0 - 10 - 20 -20 -50 -15000 1500
+float - real/ponto flutuante - 0.0 -10.10
+bool - booleano/lógico - True/False 10 == 10
+"""
+print('Luiz', type('Luiz'))
+print(10, type(10))
+print(25.23, type(25.23))
+print(10 == 10, type(10 == 10))
+
+# String: nome
+print('Camila', type('Camila'))
+
+# Idade: int
+print(27, type(27))
+
+# Altura: float
+print(1.58, type(1.58))
+
+# É maior de idade 10 > 20
+print(27 > 18, type(27 > 18))
